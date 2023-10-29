543d1caddcad84de308caccc749abbb62f13509b
AndyVx <andrew.vokhmin@gmail.com>
2023-10-29 20:27:03 +0200

    adds doc links

--- Robots/AVx Momentum Trading Bot/TestProject1/SignalLevelsDataSet.cs
+++ Robots/AVx Momentum Trading Bot/TestProject1/SignalLevelsDataSet.cs
@@ -6,15 +6,18 @@ using Microsoft.VisualStudio.TestPlatform.ObjectModel.Client;
 
 public class SignalLevelsDataSet
 {
-
-    Bar bar;
+    private double HiPercentile = 70;
+    private double LoPercentile = 30;
+    List<Bar> bars = new List<Bar>();
     public Double[] volatility;
 
     public SignalLevelsDataSet InitByLastBars(Bars bars, int Count)
     {
+        this.bars = new List<Bar>(Count);
         volatility = new double[Count];
         for (int i = 0; i < Count; i++)
         {
+            this.bars.Add(bars.Last(i));
             var index = Count - i + 1;
             volatility[i] = (bars.HighPrices.Last(index) - bars.LowPrices.Last(index)) / 2;
         }
